aae69d895d9cedca175c7e88651da4a1ba80b7ab
usfbelhadj <usf.belhadj@gmail.com>
2020-12-19 19:35:39 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,7 @@ from os import path
 
 
 if __name__ == "__main__":
+    line_list = []
     '''the number of arguments is less than 2'''
     if len(sys.argv) != 3:
         print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
@@ -13,3 +14,16 @@ if __name__ == "__main__":
     if not path.exists(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
+    '''Headings Markdown'''
+    with open(sys.argv[1], 'r') as read_file:
+        for lines in read_file.readlines():
+            cout_cra = 0
+            for line in lines:
+                for car in range(len(line)):
+                    if line[car] == '#':
+                        cout_cra += 1
+            lines = lines.rstrip('\r\n')
+            line_list.append("<h{}>{}</h{}>".format(cout_cra, lines.replace('#',''), cout_cra))
+        with open(sys.argv[2], 'a') as write_file:
+            for line in line_list:
+                write_file.write('{}\n'.format(line))
