cc720a8edbcf6a40271d38a5649ecdd1a0323d44
usfbelhadj <usf.belhadj@gmail.com>
2020-12-19 10:56:17 +0100

    Test

--- markdown2html.py
+++ markdown2html.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
 
 import sys
-import glob
+from os import path
 
 
 if __name__ == "__main__":
-    list_file = []
-    if len(sys.argv) < 2:
-        print("Usage: ./markdown2html.py README.md README.html ", file=sys.stderr)
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html")
         exit(1)
-    for file in glob.glob("*.*"):
-        list_file.append(file)
     for i in range(1, len(sys.argv)):
-        if sys.argv[i] not in list_file:
-            print("Missing {}".format(sys.argv[i]))
-            exit(1)
-    h = 0
-    x = ''
-    m = ''
-    st = ''
-    t = []
-    with open(sys.argv[1], "r") as main_file:
-        for line in main_file.readlines():
-            for c in line:
-                if c == '#':
-                    h+=1
-                if c !='#':
-                    st+=c
-                t = st.split('\n')
-            m += str(h)
-            h = 0
-        for i in range(len(t)):
-            with open(sys.argv[2], "a") as put_file:
-                put_file.write("<h{}>{}<h{}/>".format(1*(int(x) for j in m),t[i], 1*(int(x) for j in m))
+        path_file = f"/home/djo/Specializations/Markdown2HTML/{sys.argv[i]}"
+        if path.exists(path_file) == True:
+            print(f"Missing {sys.argv[i]}")
+        exit(1)
