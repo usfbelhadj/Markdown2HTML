7c1ba5794c49b6ad42b9fe52af10f4f98e713f8d
usfbelhadj <usf.belhadj@gmail.com>
2020-12-19 19:08:13 +0100

    Test markdown

--- markdown2html.py
+++ markdown2html.py
@@ -7,7 +7,7 @@ from os import path
 if __name__ == "__main__":
     '''the number of arguments is less than 2'''
     if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.mdREADME.html", file=sys.stderr)
+        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
         exit(1)
     '''Markdown file doesn’t exist'''
     if not path.exists(sys.argv[1]):
