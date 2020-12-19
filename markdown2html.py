ae7f1b74618f8a75b56383739a7798c20fb4ba38
usfbelhadj <usf.belhadj@gmail.com>
2020-12-19 10:59:59 +0100

    Test Markdown

--- markdown2html.py
+++ markdown2html.py
@@ -6,10 +6,10 @@ from os import path
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     for i in range(1, len(sys.argv)):
         path_file = f"/home/djo/Specializations/Markdown2HTML/{sys.argv[i]}"
-        if path.exists(path_file) == True:
-            print(f"Missing {sys.argv[i]}")
+        if path.exists(path_file) == False:
+            print(f"Missing {sys.argv[i]}", file=sys.stderr)
         exit(1)
