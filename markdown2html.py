22d24531386bc51cf282252c043b059086066f3e
usfbelhadj <usf.belhadj@gmail.com>
2020-12-19 11:01:47 +0100

    Test Markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
+''' Markdown to HTML '''
 import sys
 from os import path
 
 
 if __name__ == "__main__":
+    '''the number of arguments is less than 2'''
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
+    '''Markdown file doesn’t exist'''
     for i in range(1, len(sys.argv)):
         path_file = f"/home/djo/Specializations/Markdown2HTML/{sys.argv[i]}"
         if path.exists(path_file) == False:
             print(f"Missing {sys.argv[i]}", file=sys.stderr)
-        exit(1)
+            exit(1)
